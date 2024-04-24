32870c56b51e7c1d0c8259fc7d53940103b00de9
jicking <7297029+jicking@users.noreply.github.com>
2024-04-24 16:23:01 +0800

    load and render dummy data

--- src/app/routes/contact-details/contact-details.component.ts
+++ src/app/routes/contact-details/contact-details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-contact-details',
   templateUrl: './contact-details.component.html',
-  styleUrl: './contact-details.component.scss'
+  styleUrl: './contact-details.component.scss',
 })
-export class ContactDetailsComponent {
+export class ContactDetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    // this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+    console.log(`fetch contact with id: ${id}`);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/routes/contact-list/contact-list.component.ts
+++ src/app/routes/contact-list/contact-list.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../models/contact';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.scss'
+  styleUrl: './contact-list.component.scss',
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [];
 
+  // constructor(private contactService: ContactService) {}
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(): void {
+    // this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+    this.contacts = [
+      { id: 1, name: 'test1' },
+      { id: 2, name: 'test2' },
+      { id: 3, name: 'test3' },
+      { id: 4, name: 'test4' },
+    ];
+  }
 }
