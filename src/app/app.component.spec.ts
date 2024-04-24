40041edbf1a0f10f05a584555f9c5e7484669c6d
jicking <7297029+jicking@users.noreply.github.com>
2024-04-24 15:39:15 +0800

    fix ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, contact-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Contact Manager'
+    );
   });
 });
