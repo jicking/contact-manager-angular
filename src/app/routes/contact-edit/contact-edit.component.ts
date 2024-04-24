8cd50db2eff3654befe82a8eb2423a1a1557c71d
jicking <7297029+jicking@users.noreply.github.com>
2024-04-24 15:07:09 +0800

    add pages

---
+++ src/app/routes/contact-edit/contact-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-edit',
+  templateUrl: './contact-edit.component.html',
+  styleUrl: './contact-edit.component.scss'
+})
+export class ContactEditComponent {
+
+}
